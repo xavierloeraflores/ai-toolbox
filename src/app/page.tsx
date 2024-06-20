dd13d7ab3029cbae1872708d35169c24f15ba892
xavierloeraflores <x.loera.tech@gmail.com>
2024-06-19 20:34:40 -0700

    Basic Chat implementation on the homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,26 @@
+"use client";
+import { useChat } from "@ai-sdk/react";
 export default function HomePage() {
+  const { messages, input, handleInputChange, handleSubmit } = useChat();
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
-      Hello World
+      <div className="stretch mx-auto flex w-full max-w-md flex-col py-24">
+        {messages.map((m) => (
+          <div key={m.id} className="whitespace-pre-wrap">
+            {m.role === "user" ? "User: " : "AI: "}
+            {m.content}
+          </div>
+        ))}
+
+        <form onSubmit={handleSubmit}>
+          <input
+            className="fixed bottom-0 mb-8 w-full max-w-md rounded border border-gray-300 p-2 shadow-xl"
+            value={input}
+            placeholder="Say something..."
+            onChange={handleInputChange}
+          />
+        </form>
+      </div>
     </main>
   );
 }
