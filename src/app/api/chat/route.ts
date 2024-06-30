a391afa6168d93dfa527a044e985a60896e69fbc
Xavier Loera Flores <x.loera.tech@gmail.com>
2024-06-30 00:47:17 -0700

    FIX: Created ChatResponse interface close #15 Fix unsafe any assignment

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,20 +1,23 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { openai } from "@ai-sdk/openai";
+import { type CoreMessage } from "ai";
 import { streamText } from "ai";
+interface ChatResquest {
+    messages: CoreMessage[];
+}
 
 export const maxDuration = 30;
 
 export async function POST(req: Request) {
-  try {
-    const { messages } = await req.json();
+    try {
+        const { messages } = (await req.json()) as ChatResquest;
 
-    const result = await streamText({
-      model: openai("gpt-4-turbo"),
-      messages,
-    });
+        const result = await streamText({
+            model: openai("gpt-4-turbo"),
+            messages,
+        });
 
-    return result.toAIStreamResponse();
-  } catch (error) {
-    console.error(error);
-  }
+        return result.toAIStreamResponse();
+    } catch (error) {
+        console.error(error);
+    }
 }
