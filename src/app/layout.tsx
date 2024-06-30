974fe1f2de6069fed52ff67cb6ab4996f70f8319
Xavier Loera Flores <x.loera.tech@gmail.com>
2024-06-30 00:08:52 -0700

    marked layout props as readonly

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,29 +4,29 @@ import { ThemeProvider } from "~/components/theme-provider";
 import { Header } from "~/components/header";
 
 export const metadata = {
-  title: "",
-  description: "",
-  icons: [{ rel: "icon", url: "/favicon.ico" }],
+    title: "",
+    description: "",
+    icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
 export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="en" className={`${GeistSans.variable}`}>
-      <body>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <Header />
-          {children}
-        </ThemeProvider>
-      </body>
-    </html>
-  );
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html lang="en" className={`${GeistSans.variable}`}>
+            <body>
+                <ThemeProvider
+                    attribute="class"
+                    defaultTheme="system"
+                    enableSystem
+                    disableTransitionOnChange
+                >
+                    <Header />
+                    {children}
+                </ThemeProvider>
+            </body>
+        </html>
+    );
 }
